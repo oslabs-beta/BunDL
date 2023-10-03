cc7c2fc4807132bbcb2ee94e12c7ab58911ae0ef
Ken <keniwane@gmail.com>
2023-10-03 10:19:31 -0700

    bun setup

---
+++ server/server.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+const app = require('express')();
+
+const PORT = 3000;
+
+//import static files
+app.use(express.static(path.join(__dirname, '../client')));
+
+//parse request body
+app.use(express.json());
+app.use(cors());
+
+// WILL ADD DEMO ENDPOINTS HERE
+// THIS IS A PLACEHOLDER FOR DEMO ENDPOINTS
+
+//404 error handlers
+app.use((req, res) => {
+  res.status(404).json('This is a 404 error');
+});
+
+//global error handlers
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown error',
+    status: 500,
+    message: { err: 'An error occured' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+//app listen to port
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
