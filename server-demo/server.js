39fd82d6923a93ed2e2d1d73b86ddcf9287cd1f6
Andrew <apwicker@gmail.com>
2023-10-05 10:19:00 -0700

    Co-authored-by: Brandon Do <brandonndo@users.noreply.github.com> Co-authored-by: Shi Kuang <shi-kuang@users.noreply.github.com>
    Refactor server file structure and error handling
    
    Co-authored-by: Brandon Do <brandonndo@users.noreply.github.com>
    Co-authored-by: Shi Kuang <shi-kuang@users.noreply.github.com>

--- server-demo/server.js
+++ server-demo/server.js
@@ -1,15 +1,11 @@
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
-// const { graphqlHTTP } = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
 
 const app = require('express')();
 const schema = require('./schema.js');
 import interceptQueryAndParse from '../middleware/intercept-and-parse-logic.js';
-import { ApolloServer } from 'apollo-server-express';
-const { typeDefs, resolvers } = require('./schema');
-
-const server = new ApolloServer({ typeDefs, resolvers });
 
 const URI = 'https://spacex-production.up.railway.app/';
 const PORT = 3000;
@@ -21,41 +17,32 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.use(express.json());
 app.use(cors());
 
-async function startServer() {
-  await server.start();
+// Intercept requests sent to 'graphql' endpoint
+app.use('/graphql', interceptQueryAndParse);
+// WILL ADD DEMO ENDPOINTS HERE
+// THIS IS A PLACEHOLDER FOR DEMO ENDPOINTS
+app.get('/test', (req, res) => {
+  res.send('🐱 This is a test route! 🚀');
+});
 
-  // Intercept requests sent to 'graphql' endpoint
-  app.use('/graphql', interceptQueryAndParse, (req, res) => {
-    console.log(req.body.query);
-    server.applyMiddleware({ app });
-  });
-  // WILL ADD DEMO ENDPOINTS HERE
-  // THIS IS A PLACEHOLDER FOR DEMO ENDPOINTS
-  app.get('/test', (req, res) => {
-    res.send('🐱 This is a test route! 🚀');
-  });
+//404 error handlers
+app.use((req, res) => {
+  res.status(404).json('This is a 404 error');
+});
 
-  //404 error handlers
-  app.use((req, res) => {
-    res.status(404).json('This is a 404 error');
-  });
+//global error handlers
 
-  //global error handlers
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown error',
+    status: 500,
+    message: { err: 'An error occured' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  return res.status(errorObj.status).json(errorObj.message);
+});
 
-  app.use((err, req, res, next) => {
-    const defaultErr = {
-      log: 'Express error handler caught unknown error',
-      status: 500,
-      message: { err: 'An error occured' },
-    };
-    const errorObj = Object.assign({}, defaultErr, err);
-    return res.status(errorObj.status).json(errorObj.message);
-  });
-
-  //app listen to port
-  app.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}...`);
-  });
-}
-
-startServer();
+//app listen to port
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
