2a82b712584c8b88ec6f1593439b2236ed63b6e8
Andrew <apwicker@gmail.com>
2023-10-05 16:56:13 -0700

    Co-authored-by: Brandon Do <brandonndo@users.noreply.github.com> Co-authored-by: Shi Kuang <shi-kuang@users.noreply.github.com> Co-authored-by: GMogi <GioMogi@users.noreply.github.com>
    **feat**: Add email field to middleware/astOutput.json
    
    The code change in middleware/astOutput.json adds a new "email" field to the AST output. This field represents the email address of a person. The code change includes the necessary modifications to the AST structure and updates the start and end positions of the affected fields.
    
    Co-authored-by: Brandon Do <brandonndo@users.noreply.github.com>
    Co-authored-by: Shi Kuang <shi-kuang@users.noreply.github.com>
    Co-authored-by: GMogi <GioMogi@users.noreply.github.com>

--- middleware/queryObjectFunctions.js
+++ middleware/queryObjectFunctions.js
@@ -1,19 +1,56 @@
-const recreateQueryForDatabase = (proto) => {
+/**
+ * Iterates through passed-in proto object, removing fields retrieved from the Redis cache.
+ * The returned object is intended to be passed to `convertQueryObjToString` for further processing.
+ * @param {Object} proto
+ * @returns object
+ */
+const filterOutCachedResults = (proto) => {
   const dbQueryObj = {};
   for (const key in proto) {
     const reducedProto = this.toggleProtoFieldsToFalse(proto[key]);
+    if (reducedProto.length > 0) dbQueryObj[key] = reducedProto;
   }
+  return dbQueryObj;
 };
 
-const toggleProtoFieldsToFalse = (proto) => {};
+const toggleProtoFieldsToFalse = (proto) => {
+  const fields = [];
+  for (const key in proto) {
+    if (proto[key] === false) fields.push(key);
+    if (typeof proto[key] === 'object') {
+      const nestedObj = {};
+      const reducedProto = this.toggleProtoFieldsToFalse(proto[key]);
+      if (reducedProto.length > 0) {
+        nestedObj[key] = reducedProto;
+        fields.push(nestedObj);
+      }
+    }
+  }
+  return fields;
+};
 
 const convertQueryObjectToString = (queryObject) => {};
 
 const stringifyQuery = (fieldsArray) => {};
 
 module.exports = {
-  recreateQueryForDatabase,
+  filterOutCachedResults,
   toggleProtoFieldsToFalse,
   convertQueryObjectToString,
   stringifyQuery,
 };
+
+const person = {
+  firstName: 'Garth',
+  lastName: 'Emard',
+  email: 'Sadie.Roob40@yahoo.com',
+  address: {
+    street: '7036 Ida Stream',
+    city: 'East Orange',
+    state: 'North Carolina',
+    zip: '81725-4936',
+    country: 'Georgia',
+  },
+};
+
+console.log(filterOutCachedResults(person));
--- server/src/helpers/redisConnection.js
+++ server/src/helpers/redisConnection.js
@@ -1,4 +1,4 @@
-const Redis = require ('ioredis');
+const Redis = require('ioredis');
 /* 
   import { RedisClientType } from "redis";
   import { createClient } from "redis";
@@ -9,9 +9,12 @@ const Redis = require ('ioredis');
 Where do we store the REDIS_HOST & REDIS_PASSWORD? Do we need a bun.env folder? 
 */
 
-const redisPort = Number(bun.env.REDIS_PORT);
-const redisHost = bun.env.REDIS_HOST;
-const redisPassword = bun.env.REDIS_PASSWORD;
+// const redisPort = Number(bun.env.REDIS_PORT);
+// const redisHost = bun.env.REDIS_HOST;
+// const redisPassword = bun.env.REDIS_PASSWORD;
+// manually set port and host for now
+const redisPort = 6379;
+const redisHost = '127.0.0.1';
 
 const redisCacheMain = new Redis({
   host: redisHost,
@@ -19,12 +22,12 @@ const redisCacheMain = new Redis({
   password: redisPassword,
 });
 
-redisCacheMain.on("error", (error) => {
+redisCacheMain.on('error', (error) => {
   console.error(`Error when trying to connect to redisCacheMain: ${error}`);
 });
 
-redisCacheMain.on("connect", () => {
-  console.log("Connected to redisCacheMain");
+redisCacheMain.on('connect', () => {
+  console.log('Connected to redisCacheMain');
 });
 
 modeul.exports = redisCacheMain;
