3f33a17aa5e598a09e433281486c4cb0ce11b90b
Ken <keniwane@gmail.com>
2023-10-05 18:40:31 -0700

    MVP proto

--- middleware/prototype-logic.js
+++ middleware/prototype-logic.js
@@ -1,94 +1,144 @@
+// async query(req, res, next) {
 
+//   const {proto, operationType, frags} = res.locals.parsed AST ? res.locals.parsed AST : extractAST(AST)
 
-async query(req, res, next) {
+//   const prototype = Object.keys(frags).length > 0 ? addProtoWithFrag(proto,frags) : proto;
 
-  const {proto, operationType, frags} = res.locals.parsed AST ? res.locals.parsed AST : extractAST(AST)
+//   const cacheKey = JSON.stringify(prototype);
 
-  const prototype = Object.keys(frags).length > 0 ? addProtoWithFrag(proto,frags) : proto;
-
-  const cacheKey = JSON.stringify(prototype);
-
-  const cachedData = cache[cacheKey];
-  if(cachedData){
-    res.locals.data = cachedData;
-    return next();
-  }
-}
-
-function extractAST(AST){
-  let operationType = '',
+//   const cachedData = cache[cacheKey];
+//   if(cachedData){
+//     res.locals.data = cachedData;
+//     return next();
+//   }
+// }
+function extractAST(AST) {
+  let operationType = '';
   const path = [];
   const proto = {
     fields: {},
     frags: {},
-    operation: '',
+    fragsDefinitions: {},
+    operationType: '',
+    fragmentType: '',
   };
 
   visit(AST, {
     OperationDefinition(node) {
       operationType = node.operation;
       proto.operation = operationType;
+
+      if (node.selectionSet.selections[0].typeCondition) {
+        proto.operationType =
+          node.selectionSet.selections[0].typeCondition.name.value;
+      } else {
+        proto.operationType = node.selectionSet.selections[0].name.value;
+      }
+
       if (operationType === 'subscription') {
         operationType = 'noBuns';
-        BREAK;  // halt traversal
+        return BREAK;
       }
     },
     Argument(node) {
       if (node.value.kind === 'Variable' && operationType === 'query') {
         operationType = 'noBuns';
-        BREAK;  // halt traversal
+        return BREAK;
       }
+
+      function deepCheckArg(arg) {
+        if (['NullValue', 'ObjectValue', 'ListValue'].includes(arg.kind)) {
+          operationType = 'noBuns';
+          return BREAK;
+        }
+
+        if (arg.kind === 'ObjectValue') {
+          for (const field of arg.fields) {
+            return deepCheckArg(field.value);
+          }
+        }
+      }
+
+      return deepCheckArg(node.value);
+    },
+    Directive() {
+      operationType = 'noBuns';
+      return BREAK;
     },
     Field: {
       enter(node) {
-        const fieldName = node.alias ? node.alias.value : node.name.value;
-  
-        if (node.name.value.includes('__')) {
-          operationType = 'noBuns';
-          BREAK;  // halt traversal
+        const fieldName = node.alias ? node.name.value : node.name.value;
+        path.push(fieldName);
+
+        function setNestedProperty(obj, pathArray, value) {
+          let current = obj;
+          for (let i = 0; i < pathArray.length - 1; i++) {
+            if (!current[pathArray[i]]) current[pathArray[i]] = {};
+            current = current[pathArray[i]];
+          }
+          current[pathArray[pathArray.length - 1]] = value;
         }
 
-        proto.fields[fieldName] = true;
-  
-        // Navigate to the current level in the proto based on the path.
-        let currentLevel = proto.fields;
-        for (const level of path) {
-          currentLevel = currentLevel[level];
-        }
-        const fieldObj = {
-          arguments: {},
+        // Metadata collection
+        const fieldMetadata = {
+          name: node.name.value,
+          args: node.arguments
+            ? node.arguments.map((arg) => ({
+                name: arg.name.value,
+                value: arg.value.value, // Note: This assumes simple argument types for clarity.
+              }))
+            : [],
+          alias: node.alias ? node.alias.value : null,
+          type: null, // This is just a placeholder; you'd need introspection or schema analysis to get actual types.
         };
-        node.arguments.forEach((arg) => {
-          fieldObj.arguments[arg.name.value] = arg.value.value;
-        });
 
-        // Initialize a new object for the field in the proto.
-        currentLevel[fieldName] = fieldObj;
-        path.push(fieldName);  // Push current field to path.
+        if (['id', '_id', 'ID', 'Id'].includes(node.name.value)) {
+          setNestedProperty(proto.fields, path, { id: true });
+        } else {
+          setNestedProperty(proto.fields, path, {});
+        }
       },
       leave() {
-        path.pop();  // Pop the current field from path.
+        path.pop();
+      },
+    },
+    FragmentDefinition(node) {
+      proto.fragsDefinitions[node.name.value] = {};
+      for (const field of node.selectionSet.selections) {
+        if (field.kind !== 'InlineFragment') {
+          proto.fragsDefinitions[node.name.value][field.name.value] = true;
+        }
       }
     },
+
     FragmentSpread(node) {
-      proto.frags[node.name.value] = true;
+      if (proto.fragsDefinitions[node.name.value]) {
+        const fragmentFields = proto.fragsDefinitions[node.name.value];
+        for (let fieldName in fragmentFields) {
+          setNestedProperty(proto.fields, path.concat([fieldName]), true);
+        }
+      }
     },
     SelectionSet: {
       enter(node, key, parent) {
-        
-      }
-    }
+        if (parent.kind === 'InlineFragment') {
+          proto.fragmentType = parent.typeCondition.name.value;
+        }
+      },
+      leave() {
+        path.pop();
+      },
+    },
   });
 
+  if (
+    !proto.fields.id &&
+    !proto.fields._id &&
+    !proto.fields.ID &&
+    !proto.fields.Id
+  ) {
+    operationType = 'noID';
+  }
+
   return { proto, operationType };
-}
-
-const cache = {};
-// added this just for me to visualize it better
-
-function addProtoWithFrag(proto, frags) {
-
-
-  
-  return proto;
 }
