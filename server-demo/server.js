499e21f3fcf4a186bf9a3e31c12924a801e4f418
Ken <keniwane@gmail.com>
2023-10-05 18:42:47 -0700

    deleted function

--- server-demo/server.js
+++ server-demo/server.js
@@ -1,17 +1,8 @@
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
-// const { graphqlHTTP } = require('express-graphql');
-
 const app = require('express')();
-const schema = require('./schema.js');
-import interceptQueryAndParse from '../middleware/intercept-and-parse-logic.js';
-import { ApolloServer } from 'apollo-server-express';
-const { typeDefs, resolvers } = require('./schema');
 
-const server = new ApolloServer({ typeDefs, resolvers });
-
-const URI = 'https://spacex-production.up.railway.app/';
 const PORT = 3000;
 
 //import static files
@@ -21,40 +12,27 @@ app.use(express.static(path.join(__dirname, '../client')));
 app.use(express.json());
 app.use(cors());
 
-async function startServer() {
-  await server.start();
+// WILL ADD DEMO ENDPOINTS HERE
+// THIS IS A PLACEHOLDER FOR DEMO ENDPOINTS
 
-  // Intercept requests sent to 'graphql' endpoint
-  app.use('/graphql', interceptQueryAndParse);
-  server.applyMiddleware({ app });
+//404 error handlers
+app.use((req, res) => {
+  res.status(404).json('This is a 404 error');
+});
 
-  // WILL ADD DEMO ENDPOINTS HERE
-  // THIS IS A PLACEHOLDER FOR DEMO ENDPOINTS
-  app.get('/test', (req, res) => {
-    res.send('🐱 This is a test route! 🚀');
-  });
+//global error handlers
 
-  //404 error handlers
-  app.use((req, res) => {
-    res.status(404).json('This is a 404 error');
-  });
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown error',
+    status: 500,
+    message: { err: 'An error occured' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  return res.status(errorObj.status).json(errorObj.message);
+});
 
-  //global error handlers
-
-  app.use((err, req, res, next) => {
-    const defaultErr = {
-      log: 'Express error handler caught unknown error',
-      status: 500,
-      message: { err: 'An error occured' },
-    };
-    const errorObj = Object.assign({}, defaultErr, err);
-    return res.status(errorObj.status).json(errorObj.message);
-  });
-
-  //app listen to port
-  app.listen(PORT, () => {
-    console.log(`Server listening on port: ${PORT}...`);
-  });
-}
-
-startServer();
+//app listen to port
+app.listen(PORT, () => {
+  console.log(`Server listening on port: ${PORT}...`);
+});
