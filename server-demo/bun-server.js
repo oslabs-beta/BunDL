49091b000466816823de915f22a0533e586cedea
Andrew <apwicker@gmail.com>
2023-10-09 07:45:17 -0700

    Refactor response handling and remove unnecessary console.log statements
    - Refactored response handling in the '/' route in `bun-server.js`
    - Removed unnecessary `console.log` statements in the code.

--- server-demo/bun-server.js
+++ server-demo/bun-server.js
@@ -28,17 +28,15 @@ const bundlCache = new BunDL(
 
 const handlers = {
   '/': (req) => {
-    return new Response(Bun.file('../front-end/front-end/public/index.html'))
-      .then(
-        (contents) =>
-          new Response(contents, { headers: { 'Content-Type': 'text/html' } })
-      )
-      .catch((err) => new Response('File not found', { status: 404 }));
+    return new Response(Bun.file('../front-end/front-end/public/index.html'));
+    // .then(
+    //   (contents) =>
+    //     new Response(contents, { headers: { 'Content-Type': 'text/html' } })
+    // )
+    // .catch((err) => new Response('File not found', { status: 404 }));
   },
   '/graphql': (req) => {
-    console.log('the req in /graphql handler is: ', req);
     if (req.method === 'POST') {
-      console.log('inside req.method if block');
       return bundlCache.query(req).then((queryResults) => {
         return new Response(JSON.stringify(queryResults), { status: 200 });
       });
@@ -53,23 +51,11 @@ Bun.serve({
   hostname: 'localhost',
   port: 3000,
   async fetch(req) {
-    console.log(req);
-    console.log(req.url);
-    console.log(req.body);
     const data = await req;
-    console.log('data is:', data);
 
-    // const urlPath = new URL(req.url).pathname;
-    // if (urlPath === '/graphql' && req.method === 'POST') {
-    //   console.log(data);
-    //   return bundlCache.query({ body: data }).then((queryResults) => {
-    //     return new Response(JSON.stringify(queryResults), { status: 200 });
-    //   });
-    // }
     const handler = handlers[new URL(req.url).pathname];
 
     if (handler) {
-      console.log('line 69: ', data);
       return handler(req);
     }
     return new Response('This is a 404 error', { status: 404 });
