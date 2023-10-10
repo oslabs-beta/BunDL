c40888ff2ba2d5f671549740a1e46dd2b2b1a2ad
Andrew <apwicker@gmail.com>
2023-10-10 14:58:46 -0700

    Delete bunDL-client files and dependencies
    This commit removes the bunDL-client files and related dependencies. The deleted files include ".gitignore", "README.md", "bunDL-client.js", "index.html", and "package.json". The commit also removes the PouchDB-related code from bunDL-client.js, including the import statements, plugin registration, and creating a PouchDB instance. The package.json file is also removed, which specified the name, version, dependencies, devDependencies, and scripts for the bunDL-client package.
    
    Note: Commit messages need to be in the imperative tense rather than describing actions in the past tense.

--- bunDL-client/bunDL-client.js
+++
@@ -1,6 +0,0 @@
-import PouchDb from 'pouchdb-browser';
-import PouchDbIdbAdapter from 'pouchdb-adapter-idb';
-
-PouchDb.plugin(PouchDbIdbAdapter);
-const db = new PouchDb('localdb', { adapter: 'idb' });
-console.log(db.adapter);
