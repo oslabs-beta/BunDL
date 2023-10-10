6cd3d8440bc106dd588d0c76dd0731cf8aeb3523
Andrew <apwicker@gmail.com>
2023-10-10 16:48:32 -0700

    feat: Add BunCache database integration
    This commit adds the ability to integrate a database into the BunCache class. The `BunCache` constructor now accepts an optional `database` parameter, which defaults to a new instance of the `Database` class. The `Database` class is also included in this commit, providing methods for checking the status of the database, storing documents, retrieving documents by ID, and updating documents.
    
    feat: Add database integration to Database class
    
    This commit adds the ability to integrate a database into the Database class. The constructor now accepts a parameter to determine the database name or web address. The class includes methods for checking the status of the database, storing documents, retrieving documents by ID, updating documents, and deleting documents. Additionally, there is a commented-out method for performing bulk operations and a method for resolving and updating documents with conflicts.

--- client/src/helpers/pouchHelpers.js
+++ client/src/helpers/pouchHelpers.js
@@ -2,6 +2,7 @@ const PouchDB = require('pouchdb');
 
 export default class Database {
   constructor(databaseName) {
+    // ToDo: parse argument to determine if web address or name
     this.db = new PouchDB(databaseName);
     // this.db = new PouchDB('http://path.to/couchdb/here')
   }
@@ -11,7 +12,8 @@ export default class Database {
     try {
       const info = await this.db.info();
       console.log(
-        `The current state of the ${this.db.db_name} database is: ${info}`
+        `The current state of the ${this.db.name} database is: `,
+        info
       );
     } catch (error) {
       console.error(error);
@@ -23,36 +25,76 @@ export default class Database {
     try {
       await this.db.put(document);
     } catch (error) {
-      return console.error(error);
+      console.error(error);
     }
   }
 
+  // RETRIEVE DOCUMENT FROM POUCHDB
   async retrieveDocument(id) {
     try {
-      this.db.get(id);
-      await function (doc) {
-        console.log(doc);
-      };
+      const doc = await this.db.get(id);
+      console.log(doc);
     } catch (error) {
       console.error(error);
     }
   }
 
+  // UPDATE DOCUMENT IN POUCHDB
   async updateDocument(id, field, value) {
     try {
-      this.db.get(id);
-      await function (doc) {
-        doc[field] = value;
-        return this.db.put(doc);
-      };
-      await function () {
-        return this.db.get(id);
-      };
-      await function (doc) {
-        console.log(doc);
-      };
+      let doc = await this.db.get(id);
+      doc[field] = value;
+      await this.db.put(doc);
+
+      // To verify updated document
+      doc = await this.db.get(id);
+      console.log(doc);
     } catch (error) {
       console.error(error);
+    }
+  }
+
+  // DELETE A DOCUMENT FROM POUCHDB
+  async deleteDocument(id) {
+    try {
+      const doc = await this.db.get(id);
+      const response = await this.db.remove(doc);
+      console.log('Document deleted: ', response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  // BULK OPERATIONS (STORE, UPDATE, DELETE)
+  // async performBulkOperations(docs) {
+  //   try {
+  //     const response = await this.db.bulkDocs(docs);
+  //     console.log(('Bulk operations success: ', response));
+  //   } catch (error) {
+  //     console.error('Bulk operation failed:', error);
+  //   }
+  // }
+
+  // RESOLVE CONFLICTS
+  async resolveAndUpdateDocument(id, field, value) {
+    try {
+      const doc = await this.db.get(id, {
+        conflicts: true,
+      });
+      if (doc._conflicts && doc._conflicts.length) {
+        // todo: need conflicts resolution functions here
+
+        await Promise.all(
+          doc._conflicts.map((rev) => {
+            this.db.remove(id, rev);
+          })
+        );
+      }
+      doc[field] = value;
+      await this.db.put(doc);
+      console.log('Document updated with conflict resolution.');
+    } catch (error) {
+      console.error('Could not update document: ', error);
     }
   }
 }
