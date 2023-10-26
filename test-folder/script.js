3ca812aa34a46fd3fc17298f7ed74a69991a9d2f
Ken <keniwane@gmail.com>
2023-10-25 21:44:13 -0700

    bun build

--- test-folder/script.js
+++ test-folder/script.js
@@ -1,1 +1,36 @@
 import bunDLClient from 'bundl-client';
+
+const sampleSchema = `
+query {
+  company (id: "company0"){ 
+     company 
+     city 
+     state 
+     department (id: "department0") {
+         departmentName 
+         }
+     } 
+ }
+`;
+
+const bunCache = new bunDLClient(sampleSchema);
+
+async function testBunCache() {
+  // Test 1: First query (should be a cache miss)
+  const query1 = `
+    query {
+      hello
+    }
+  `;
+
+  let response = await bunCache.query(query1);
+  console.log('Test 1:', response); // Should log the GraphQL response and cache data
+
+  // Test 2: Running the same query (should be a cache hit)
+  response = await bunCache.query(query1);
+  console.log('Test 2:', response); // Should log the GraphQL response and cache data with 'cache: hit'
+
+  // Add more test queries as needed to thoroughly test your caching solution.
+}
+
+testBunCache().catch((err) => console.error('Error during test:', err));
