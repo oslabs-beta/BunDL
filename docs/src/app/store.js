620cda9d83ea29eb3b559523f88fceebf98fb28f
Andrew <apwicker@gmail.com>
2023-10-26 22:31:55 -0700

    rebuild website

--- docs/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../reducers/counterSlice';
-
-export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-  },
-});
